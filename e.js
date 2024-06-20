e187da4241eaa100ae83e411881692430e5f6665
Jair_Bolsonaro <4552129740@estudante.sed.sc.gov.br>
2024-06-19 21:35:09 -0300

    primeiros exercicios de javascriptp

---
+++ e.js
@@ -0,0 +1,96 @@
+ //ex 1        
+let nome='joão'
+console.log (nome)
+
+//ex 2
+let  a=5, b=10, soma=a+b 
+console.log(soma)
+
+//ex 3
+let primeironome='Josefa', ultimonome='Silva'
+console.log(primeironome,ultimonome)
+
+//ex 4
+let pessoa = {nome:'Eduarda',idade:16,endereço:'casa número 3671',telefone:40028922}
+console.log(pessoa)
+
+//ex 4.2
+let vetor=['maçã', 'banana', 'laranja']
+vetor.push('uva')
+console.log (vetor)
+
+//ex 5
+let numeros=[1,2,3,4,5]
+console.log(numeros[0])
+console.log(numeros[4])
+
+//ex 6
+
+let cores=['vermelho', 'verde', 'azul']
+
+for(let i=0; i< cores.length; i++){
+    console.log (cores[i])
+    }
+    
+//ex 7    
+
+let idade=17
+if(idade>=18){
+  console.log ('/nMaior de idade') 
+}else {console.log('menor de idade')
+}
+//ex 8
+let nota=3
+if (nota>=7) {console.log('\naprovado')}
+  if  (nota>=5 && nota<7){console.log('\nrecuperação')}
+  else{console.log('\nreprovado')}
+
+//ex 9  Declare uma variável chamada diaDaSemana e atribua a ela um valor de 1 a 7 representando os dias da semana. Use a estrutura switch para exibir o nome do dia correspondente (1 para "Domingo", 2 para "Segunda-feira", etc.).
+
+let diadasemana=4, mensagem
+
+switch(diadasemana){
+   case 1:
+    mensagem='segunda-feira'
+    break
+
+  
+  case 1:
+  mensagem='segunda-feira'
+  break
+ (diadasemana)
+            case 2:
+             mensagem='segunda-feira'
+             break
+             (diadasemana)
+                case 3:
+                 mensagem='segunda-feira'
+                 break
+
+        (diadasemana){
+             case 4:
+                     mensagem='segunda-feira'
+                     break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
